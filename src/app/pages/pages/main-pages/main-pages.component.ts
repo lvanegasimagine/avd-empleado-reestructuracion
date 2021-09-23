1cd390ce341d7560c5a76530bcd70f9a367ab132
Luis Vanegas <lvanegas1429@gmail.com>
2021-09-23 14:33:35 -0600

    Dashboard Component Implementado

--- src/app/pages/pages/main-pages/main-pages.component.ts
+++ src/app/pages/pages/main-pages/main-pages.component.ts
@@ -1,36 +1,12 @@
-import { PlatformLocation } from '@angular/common';
-import { Component, NgZone, OnDestroy, OnInit } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
+
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-main-pages',
   templateUrl: './main-pages.component.html',
   styleUrls: ['./main-pages.component.css']
 })
-export class MainPagesComponent implements OnInit, OnDestroy {
+export class MainPagesComponent {
 
-  /**
-   *
-   */
-  constructor(private router: Router) {
-    // this.ciclo();
-  }
   
-  ciclo(){
-    window.location.reload();
-  }
-  ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    // this.router.navigateByUrl('/dashboard', {skipLocationChange: true}).then(()=>
-    //     this.router.navigate(["/dashboard"]));
-    //     console.log('onta');
-    // this.ciclo();
-  }
-
-  ngOnDestroy(): void {
-    //Called once, before the instance is destroyed.
-    //Add 'implements OnDestroy' to the class.
-    // this.ciclo();
-  }
 }
