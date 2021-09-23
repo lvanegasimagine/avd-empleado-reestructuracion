4f577611409c9e17a5ed2c012c597f8f1e5341fa
Luis Vanegas <lvanegas1429@gmail.com>
2021-09-23 17:16:05 -0600

    ListadoEmpleado.component funcional

--- src/app/pages/pages/empleado/listado-empleado/listado-empleado.component.ts
+++ src/app/pages/pages/empleado/listado-empleado/listado-empleado.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, OnInit } from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import { EmpleadoService } from 'src/app/pages/services/empleado.service';
 
 @Component({
   selector: 'app-listado-empleado',
   templateUrl: './listado-empleado.component.html',
   styleUrls: ['./listado-empleado.component.css']
 })
-export class ListadoEmpleadoComponent implements OnInit {
+export class ListadoEmpleadoComponent implements AfterViewInit, OnInit {
 
-  constructor() { }
-
+  empleadoList: any[] = [];
+  displayedColumns: string[] = ['nombre','inss','celular','cargo','area','tools'];
+  dataSource = new MatTableDataSource<any>(this.empleadoList);
+  
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  
+  constructor(private empleadoService: EmpleadoService) {
+  }
+  
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+  
   ngOnInit(): void {
+    this.listarEmpleado();
+    console.log('datasource',this.dataSource);
+  }
+
+
+  listarEmpleado(){
+    this.empleadoService.getEmpleadoList().subscribe(resp => {
+      this.dataSource.data = resp['data'];
+    })
+  };
+
+  deleteEmpleado(id: string){
+     this.empleadoService.deleteEmpleado(id).subscribe(resp => {
+         console.log('Eliminado');
+         this.listarEmpleado();
+     });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    console.log(filterValue);
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
