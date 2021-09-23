0d13ad2a8ba9c004b254a7240b6ef80dcdce138c
Luis Vanegas <lvanegas1429@gmail.com>
2021-09-23 14:12:15 -0600

    Footer funcional y main-pages implementado

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,13 +5,15 @@ import { PagesRoutingModule } from './pages-routing.module';
 import { MainPagesComponent } from './pages/main-pages/main-pages.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     MainPagesComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
