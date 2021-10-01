3394ac8caeb69e58ecb010c95178537b5d27f2e0
Luis Vanegas <lvanegas1429@gmail.com>
2021-10-01 13:39:34 -0600

    Implementando el canactivate y canload en dashboard y modificando la funcion validarToken

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { ValidarTokenGuard } from './guards/validar-token.guard';
 
 const routes: Routes = [
     {
@@ -9,7 +10,8 @@ const routes: Routes = [
     {
      path: 'dashboard',
      loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule),
-     // runGuardsAndResolvers: 'always'
+     canActivate: [ValidarTokenGuard],
+     canLoad: [ValidarTokenGuard]
     },
     { path: '**', redirectTo: 'auth'}
 ];
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { AuthResponse, Usuario } from '../interfaces/interface';
 import { map, catchError, tap } from 'rxjs/operators';
-import { of } from 'rxjs';
+import { of, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -41,10 +41,14 @@ export class AuthService {
     )
   }
 
-  validarToken(){
+  validarToken(): Observable<boolean>{
     const url = `${this.URL_USUARIO}/auth/renew`;
     const headers = new HttpHeaders().set('x-token', localStorage.getItem('token') || '');
 
-    return this.http.get(url, { headers });
+    return this.http.get<AuthResponse>(url, { headers }).pipe(
+      map(resp => {
+        return resp.status;
+      }),
+      catchError(errr => of(false)));
   }
 }
